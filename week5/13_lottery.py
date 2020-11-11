9df590cece4d03c6689250e420a064b4276a7465
Mark Ambrus <ambrusmark97@gmail.com>
2020-11-11 08:48:56 +0100

    almost done

--- week5/13_lottery.py
+++ week5/13_lottery.py
@@ -6,6 +6,9 @@ lottery_file = 'week5/lottery.csv'
 def five_most_frequent():
     winning_numbers = []
     occurences = {}
+    most_common = []
+    common_numbers = []
+    unique_values = []
     with open(lottery_file, 'r') as file:
         csv_reader = csv.reader(file, delimiter=';')
         for line in csv_reader:
@@ -14,9 +17,26 @@ def five_most_frequent():
             winning_numbers.append(line[13])
             winning_numbers.append(line[14])
             winning_numbers.append(line[15])
-        for i in range(1, 90):
-            num = str(i)
-            occurences[i] = winning_numbers.count(num)
-        print(occurences)
+        for number in range(1, 90):
+            num = str(number)
+            occurences[number] = winning_numbers.count(num)
+            #print(occurences)
+            number_list = list(occurences.keys())
+            #print(number_list)
+            frequency_list = list(occurences.values())
+            #print(frequency_list)
+        values = sorted(occurences.values())
+        for i in range(0, len(values) - 1):
+            if values[i] != values[i + 1]:
+                unique_values.append(values[i])
+        for i in range(0, 5):
+            most_common.append(max(unique_values))
+            unique_values.remove(max(unique_values))
+            common_numbers.append(str(number_list[frequency_list.index(most_common[i])])) #TODO create list in list to have multiple number to occurence
+        print("The most common numbers are: " + common_numbers[0])
+        print("The second most common numbers are: " + common_numbers[1])
+        print("The third most common numbers are: " + common_numbers[2])
+        print("The fourth most common numbers are: " + common_numbers[3])
+        print("The fifth most common numbers are: " + common_numbers[4])
 
 five_most_frequent()
