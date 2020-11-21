fc30182228c1f35e8663c4005cea99d25d16d6c8
Mark Ambrus <ambrusmark97@gmail.com>
2020-11-21 12:26:46 +0100

    More in-line comments, 0 index bug handling, incorrectly formatted task file scenario handling

--- week6/main.py
+++ week6/main.py
@@ -1,8 +1,10 @@
 import sys
 
 def print_usage():
+    #print header
     print("$ todo\n\nCommand Line Todo application\n")
     print("=============================\n\nCommand line arguments:")
+    #print arguments
     print("\t-l\tLists all the tasks")
     print("\t-a\tAdds a new task")
     print("\t-r\tRemoves a task")
@@ -15,7 +17,7 @@ def get_tasks():
         with open(".tasks.txt", 'r') as file:
             #read all lines
             lines = file.readlines()
-            #remove linebreaks
+            #create list without linebreaks
             tasks = []
             for i in range(len(lines)):
                 tasks.append(lines[i].replace("\n", ""))
@@ -34,7 +36,7 @@ def set_tasks(tasks):
     #open task file for writing
     try:
         with open(".tasks.txt", 'w') as file:
-            #write tasks in file
+            #enter the tasks in the file
             for task in tasks:
                 file.write(task + "\n")
             file.close()
@@ -47,7 +49,9 @@ def set_tasks(tasks):
         return "IO error"
 
 def list_tasks():
+    #get to do list
     tasks = get_tasks()
+    #check for empty to do list
     if len(tasks) == 0:
         print("No todos for today! :)")
     #print tasks with numbers attached
@@ -58,124 +62,145 @@ def list_tasks():
             current_task += 1
 
 def add_task(task):
-    #get tasks
+    #get to do list
     tasks = get_tasks()
-    #add new task
+    #add new unchecked task
     tasks.append("[ ] " + task)
+    #enter the tasks in the file
     set_tasks(tasks)
 
 def remove_task():
-    tasks = get_tasks()        #index out of range error
+    #get to do list
+    tasks = get_tasks()
+    #index out of range error
     if int(sys.argv[2]) > len(tasks):
         print("Unable to remove: index is out of bound")
     else:
+        #remove task from list
         tasks.remove(tasks[int(sys.argv[2]) - 1])
+    #enter the tasks in the file
     set_tasks(tasks)        #remove the task with the given index from file
 
 def check_task():
-    tasks = get_tasks()        #index out of range error
+    #get to do list
+    tasks = get_tasks()
+    #index out of range error
     if int(sys.argv[2]) > len(tasks):
         print("Unable to check: index is out of bound")
     else:
+        #check task
         checked_tasks = []
         checked_task = tasks[int(sys.argv[2]) - 1]
-        if checked_task[1] == "x":
-            checked_task = checked_task.replace("x", " ", 1)
-        elif checked_task[1] == " ":
-            checked_task = checked_task.replace(" ", "x", 1)
+        if checked_task[:3] == "[x]":
+            checked_task = checked_task.replace("[x]", "[ ]", 1)
+        elif checked_task[:3] == "[ ]":
+            checked_task = checked_task.replace("[ ]", "[x]", 1)
         else:
-            print("Corrupted file")
+            #file content format is not appropriate
+            print("Corrupted file. Remove the task with this index.")
+            return
         for i in range(len(tasks)):
             if i != int(sys.argv[2]) - 1:
                 checked_tasks.append(tasks[i])
             else:
                 checked_tasks.append(checked_task)
+        #enter the tasks in the file
         set_tasks(checked_tasks)
 
 def modify_task():
+    #get to do list
     tasks = get_tasks()
+    #index out of range error
     if int(sys.argv[2]) > len(tasks):
         print("Unable to modify: index is out of bound")
     else:
+        #modify text of the task
         modified_tasks = []
-        if tasks[int(sys.argv[2]) - 1][1] == "x":
+        if tasks[int(sys.argv[2]) - 1][:3] == "[x]":
             modified_task = "[x] " + sys.argv[3]
-        elif tasks[int(sys.argv[2]) - 1][1] == " ":
+        elif tasks[int(sys.argv[2]) - 1][:3] == "[ ]":
             modified_task = "[ ] " + sys.argv[3]
         else:
-            print("Corrupted file")
+            #file content format is not appropriate
+            print("Corrupted file. Remove the task with this index.")
+            return
         for i in range(len(tasks)):
             if i != int(sys.argv[2]) - 1:
                 modified_tasks.append(tasks[i])
             else:
                 modified_tasks.append(modified_task)
+        #enter the tasks in the file
         set_tasks(modified_tasks)
 
 arguments = ['-l', '-a', 'r', 'c', 'm']
-#prints commands
+#print commands
 if len(sys.argv) == 1:
     print_usage()
 #list tasks
 elif len(sys.argv) == 2 and sys.argv[1] == "-l":
     list_tasks()
-#list tasks more
+#list tasks too many arguments error
 elif len(sys.argv) > 2 and sys.argv[1] == "-l":
     print("-l does not have any arguments.")
 #add new task
 elif len(sys.argv) == 3 and sys.argv[1] == "-a":
     add_task(sys.argv[2])
-#add new task error handling
+#add new task no task error
 elif len(sys.argv) == 2 and sys.argv[1] == "-a":
     print("Unable to add: no task provided.")
-#add multiple tasks error
+#add too many arguments error
 elif len(sys.argv) > 3 and sys.argv[1] == "-a":
     print("You can only add one task at a time.")
     print("To add multiple words as one task, put them in quotation marks.")
-#remove task
-elif (len(sys.argv) == 3 and sys.argv[1] == "-r" and
-        sys.argv[2].isdigit()):
-    remove_task()
-#remove error not number
-elif (len(sys.argv) == 3 and sys.argv[1] == "-r" and
-        not sys.argv[2].isdigit()):
+#remove index not number error
+elif len(sys.argv) == 3 and sys.argv[1] == "-r" and not sys.argv[2].isdigit():
     print("Unable to remove: index is not a number.")
-#remove task no index
+#remove index zero error
+elif len(sys.argv) == 3 and sys.argv[1] == "-r" and int(sys.argv[2]) == 0:
+    print("Unable to remove: index cannot be 0.")
+#remove task
+elif len(sys.argv) == 3 and sys.argv[1] == "-r":
+    remove_task()
+#remove task no index error
 elif len(sys.argv) == 2 and sys.argv[1] == "-r":
     print("Unable to remove: no index provided.")
-#remove multiple
+#remove too many arguments error
 elif len(sys.argv) > 3 and sys.argv[1] == "-r":
-    print("You can only remove 1 task at a time.")
-#check task
-elif (len(sys.argv) == 3 and sys.argv[1] == "-c" and
-        sys.argv[2].isdigit()):
-    check_task()
-#check error not number
-elif (len(sys.argv) == 3 and sys.argv[1] == "-c" and
-        not sys.argv[2].isdigit()):
+    print("You can only remove one task at a time.")
+#check index not number error
+elif len(sys.argv) == 3 and sys.argv[1] == "-c" and not sys.argv[2].isdigit():
     print("Unable to check: index is not a number.")
-#check task no index
+#check index zero error
+elif len(sys.argv) == 3 and sys.argv[1] == "-c" and int(sys.argv[2]) == 0:
+    print("Unable to check: index cannot be 0.")
+#check task
+elif len(sys.argv) == 3 and sys.argv[1] == "-c":
+    check_task()
+#check task no index error
 elif len(sys.argv) == 2 and sys.argv[1] == "-c":
     print("Unable to check: no index provided.")
-#remove multiple
+#check too many arguments error
 elif len(sys.argv) > 3 and sys.argv[1] == "-c":
-    print("You can only check 1 task at a time.")
-#modify task
-elif (len(sys.argv) == 4 and sys.argv[1] == "-m" and
-        sys.argv[2].isdigit()):
-    modify_task()
-#modify error not number
-elif (len(sys.argv) == 4 and sys.argv[1] == "-m" and
-        not sys.argv[2].isdigit()):
+    print("You can only check one task at a time.")
+#modify index not number error
+elif len(sys.argv) == 4 and sys.argv[1] == "-m" and not sys.argv[2].isdigit():
     print("Unable to modify: index is not a number.")
+#modify index zero error
+elif len(sys.argv) == 4 and sys.argv[1] == "-m" and int(sys.argv[2]) == 0:
+    print("Unable to modify: index cannot be 0.")
+#modify task
+elif len(sys.argv) == 4 and sys.argv[1] == "-m":
+    modify_task()
 #modify task no index
 elif len(sys.argv) == 2 and sys.argv[1] == "-m":
     print("Unable to modify: no index provided.")
 #modify one argument missing
 elif len(sys.argv) == 3 and sys.argv[1] == "-m":
     print("The index or the task is missing.")
-#modify too many arguments
+#modify too many arguments error
 elif len(sys.argv) > 4 and sys.argv[1] == "-m":
     print("Too many arguments given. Enter the index then the task")
+    print("To enter multiple words as one task, put them in quotation marks.")
 #unsupported argument
 elif sys.argv[0] not in arguments:
     print("Unsupported argument")
