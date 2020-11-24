41bb19fae9925aa38ddd516a0027c36bef2328ed
Mark Ambrus <ambrusmark97@gmail.com>
2020-11-24 20:48:15 +0100

    removed 40 unnecessary comment lines

--- week6/documentation.md
+++ week6/documentation.md
@@ -1,33 +1,73 @@
+Background:
+The project was created using VS Code, git and Python using macOS.
+
+File structure:
+The project consists of 2 files outside this documentation: main.py and .tasks.txt.
+main.py was written in python and is meant to be used with Python 3.x.
+.tasks.txt stores the tasks. It is hidden to prevent unwanted modifications by the user.
+
+Required to launch:
 1. Have the repository cloned to your device.
-2. Open the terminal and navigate inside the directory/repository.
-3. Type "python3 main.py" then the argument(s) to interact with the todo app.
+2. Have Pthon 3.x installed on your device.
+3. Open the terminal and navigate inside the directory.
+4. Type "python3 main.py" then the argument(s) to interact with the todo app.
+
+
+How to use:
+If you run without arguments, it will print the usage.
 
 The arguments are the following:
 
 -l
-Lists all the tasks
+Lists all the tasks.
 It has no further arguments.
-How to use: python3 main.py -l
+Example: python3 main.py -l
 
 -a
 Adds a new task
-It has one additional argument, which is the task itself that needs to be added
-How to use: python3 main.py -a "task here"
+It has one additional argument, which is the task itself that needs to be added.
+Example: python3 main.py -a "task here"
 If the task to be added is just 1 word, the quotation marks are not required.
 
 -r
 Removes a task
-It has one additional argument, the index of the task that needs to be removed
-How to use: python3 main.py -r 1
+It has one additional argument, the index of the task that needs to be removed.
+Example: python3 main.py -r 1
 
 -c
 Checks a task
-It has one additional argument, the index of the task that needs to be checked
-How to use: python3 main.py -c 1
+It has one additional argument, the index of the task that needs to be checked.
+Example python3 main.py -c 1
 
 -m
 Modifies a task
-It has two additional arguments, the first is the index of the task that needs to be modified, the second is the new text of the task
-How to use: python3 main.py -m 1 "modified task here"
+It has two additional arguments, the first is the index of the task that needs to be modified, the second is the new text of the task.
+Example: python3 main.py -m 1 "modified task here"
 
 If the user input is incorrect, the app will display informative error messages.
+
+Code structure:
+The core of the app is an if-elif structure. The app checks for a certain scenario, then for another until all possible scenarios are checked. These are determined by the number of arguments and the content of those arguments.
+
+Unsupported arguments are not accepted.
+
+If more arguments are provided than necessay, the app lets the user know about that.
+In case of listing, the app executes the list_tasks() function, but other arguments are ignored.
+
+If less arguments are provided than necessary when modifying, the app tells the user that both the index and the text of the new task are necessary.
+
+In case of removing, checking or modifying, only the existing indexes are acceptable arguments.
+
+The NameError, FileNotFoundError and the IOError are handled when dealing with the file .tasks.txt file.
+
+All lines in the .tasks.txt should start with "[ ]" or "[x]". If for some reason the line would not start like that, when checking or modifying, the app stops, lets the user know that the file is corrupted and how the incorrectly formatted line should be deleted.
+
+The print_usage() function is activated if the app is launched without arguments. It prints the header then lists the possible arguments and what they do.
+
+The get_tasks() function creates a list of the existing tasks stored in the .tasks.txt file, while the set_tasks(tasks) function writes the list of tasks in the .tasks.txt file.
+
+The list_tasks() function lists the tasks in a numbered order, and shows which are checked and which are not.
+The add_task(task) function adds the argument as an unchecked task to the .tasks.txt file
+The remove_task() function removes a task with the given index from the tasks.txt file.
+The check_task() checks if it is unchecked, or unchecks if it is checked the task with the given index.
+The modify_task() changes the task with the given index to the given text.
--- week6/main.py
+++ week6/main.py
@@ -1,10 +1,8 @@
 import sys
 
 def print_usage():
-    #print header
     print("$ todo\n\nCommand Line Todo application\n")
     print("=============================\n\nCommand line arguments:")
-    #print arguments
     print("\t-l\tLists all the tasks")
     print("\t-a\tAdds a new task")
     print("\t-r\tRemoves a task")
@@ -12,10 +10,8 @@ def print_usage():
     print("\t-m\tModifies a task")
 
 def get_tasks():
-    #open task file for reading
     try:
         with open(".tasks.txt", 'r') as file:
-            #read all lines
             lines = file.readlines()
             #create list without linebreaks
             tasks = []
@@ -24,7 +20,6 @@ def get_tasks():
                 tasks = list(filter(None, tasks))
             file.close()
             return tasks
-    #handle exceptions
     except NameError:
         return "Name error"
     except FileNotFoundError:
@@ -33,14 +28,11 @@ def get_tasks():
         return "IO error"
 
 def set_tasks(tasks):
-    #open task file for writing
     try:
         with open(".tasks.txt", 'w') as file:
-            #enter the tasks in the file
             for task in tasks:
                 file.write(task + "\n")
             file.close()
-    #handle exceptions
     except NameError:
         return "Name error"
     except FileNotFoundError:
@@ -49,46 +41,34 @@ def set_tasks(tasks):
         return "IO error"
 
 def list_tasks():
-    #get to do list
     tasks = get_tasks()
-    #check for empty to do list
     if len(tasks) == 0:
         print("No todos for today! :)")
     #print tasks with numbers attached
     else:
-        current_task = 1
+        current_index = 1
         for task in tasks:
-            print(str(current_task) + " - " + task)
-            current_task += 1
+            print(str(current_index) + " - " + task)
+            current_index += 1
 
 def add_task(task):
-    #get to do list
     tasks = get_tasks()
-    #add new unchecked task
     tasks.append("[ ] " + task)
-    #enter the tasks in the file
     set_tasks(tasks)
 
 def remove_task():
-    #get to do list
     tasks = get_tasks()
-    #index out of range error
     if int(sys.argv[2]) > len(tasks):
         print("Unable to remove: index is out of bound")
     else:
-        #remove task from list
         tasks.remove(tasks[int(sys.argv[2]) - 1])
-    #enter the tasks in the file
     set_tasks(tasks)
 
 def check_task():
-    #get to do list
     tasks = get_tasks()
-    #index out of range error
     if int(sys.argv[2]) > len(tasks):
         print("Unable to check: index is out of bound")
     else:
-        #check task
         checked_tasks = []
         checked_task = tasks[int(sys.argv[2]) - 1]
         if checked_task[:3] == "[x]":
@@ -96,21 +76,19 @@ def check_task():
         elif checked_task[:3] == "[ ]":
             checked_task = checked_task.replace("[ ]", "[x]", 1)
         else:
-            #file content format is not appropriate
-            print("Corrupted file. Remove the task with this index.")
+            print("Corrupted file. Remove the task with the index "
+                  + sys.argv[2] + " by typing: python3 main.py -r "
+                  + sys.argv[2])
             return
         for i in range(len(tasks)):
             if i != int(sys.argv[2]) - 1:
                 checked_tasks.append(tasks[i])
             else:
                 checked_tasks.append(checked_task)
-        #enter the tasks in the file
         set_tasks(checked_tasks)
 
 def modify_task():
-    #get to do list
     tasks = get_tasks()
-    #index out of range error
     if int(sys.argv[2]) > len(tasks):
         print("Unable to modify: index is out of bound")
     else:
@@ -121,90 +99,67 @@ def modify_task():
         elif tasks[int(sys.argv[2]) - 1][:3] == "[ ]":
             modified_task = "[ ] " + sys.argv[3]
         else:
-            #file content format is not appropriate
-            print("Corrupted file. Remove the task with this index.")
+            print("Corrupted file. Remove the task with the index "
+                  + sys.argv[2] + " by typing: python3 main.py -r "
+                  + sys.argv[2])
             return
         for i in range(len(tasks)):
             if i != int(sys.argv[2]) - 1:
                 modified_tasks.append(tasks[i])
             else:
                 modified_tasks.append(modified_task)
-        #enter the tasks in the file
         set_tasks(modified_tasks)
 
 arguments = ['-l', '-a', 'r', 'c', 'm']
-#print commands
 if len(sys.argv) == 1:
     print_usage()
-#list tasks
 elif len(sys.argv) == 2 and sys.argv[1] == "-l":
     list_tasks()
-#list tasks too many arguments error
 elif len(sys.argv) > 2 and sys.argv[1] == "-l":
-    print("-l does not have any arguments.")
-#add new task
+    print("-l does not have any arguments.\n\n")
+    list_tasks()
 elif len(sys.argv) == 3 and sys.argv[1] == "-a":
     add_task(sys.argv[2])
-#add new task no task error
 elif len(sys.argv) == 2 and sys.argv[1] == "-a":
     print("Unable to add: no task provided.")
-#add too many arguments error
 elif len(sys.argv) > 3 and sys.argv[1] == "-a":
     print("You can only add one task at a time.")
     print("To add multiple words as one task, put them in quotation marks.")
-#remove index not number error
 elif len(sys.argv) == 3 and sys.argv[1] == "-r" and not sys.argv[2].isdigit():
     print("Unable to remove: index is not a number.")
-#remove index zero error
 elif len(sys.argv) == 3 and sys.argv[1] == "-r" and int(sys.argv[2]) == 0:
     print("Unable to remove: index cannot be 0.")
-#remove task
 elif len(sys.argv) == 3 and sys.argv[1] == "-r":
     remove_task()
-#remove task no index error
 elif len(sys.argv) == 2 and sys.argv[1] == "-r":
     print("Unable to remove: no index provided.")
-#remove too many arguments error
 elif len(sys.argv) > 3 and sys.argv[1] == "-r":
     print("You can only remove one task at a time.")
-#check index not number error
 elif len(sys.argv) == 3 and sys.argv[1] == "-c" and not sys.argv[2].isdigit():
     print("Unable to check: index is not a number.")
-#check index zero error
 elif len(sys.argv) == 3 and sys.argv[1] == "-c" and int(sys.argv[2]) == 0:
     print("Unable to check: index cannot be 0.")
-#check task
 elif len(sys.argv) == 3 and sys.argv[1] == "-c":
     check_task()
-#check task no index error
 elif len(sys.argv) == 2 and sys.argv[1] == "-c":
     print("Unable to check: no index provided.")
-#check too many arguments error
 elif len(sys.argv) > 3 and sys.argv[1] == "-c":
     print("You can only check one task at a time.")
-#modify index not number error
 elif len(sys.argv) == 4 and sys.argv[1] == "-m" and not sys.argv[2].isdigit():
     print("Unable to modify: index is not a number.")
-#modify index zero error
 elif len(sys.argv) == 4 and sys.argv[1] == "-m" and int(sys.argv[2]) == 0:
     print("Unable to modify: index cannot be 0.")
-#modify task
 elif len(sys.argv) == 4 and sys.argv[1] == "-m":
     modify_task()
-#modify task no index
 elif len(sys.argv) == 2 and sys.argv[1] == "-m":
     print("Unable to modify: no index provided.")
-#modify one argument missing
 elif len(sys.argv) == 3 and sys.argv[1] == "-m":
     print("The index or the task is missing.")
-#modify too many arguments error
 elif len(sys.argv) > 4 and sys.argv[1] == "-m":
     print("Too many arguments given. Enter the index then the task")
     print("To enter multiple words as one task, put them in quotation marks.")
-#unsupported argument
 elif sys.argv[0] not in arguments:
     print("Unsupported argument")
     print_usage()
-#unknown error
 else:
     print("Unknown error")
