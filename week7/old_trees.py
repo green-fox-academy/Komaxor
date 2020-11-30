044709b502255eab71d37f8f536427a7ab38f8b9
Mark Ambrus <ambrusmark97@gmail.com>
2020-11-30 15:54:54 +0100

    not with OOP

---
+++ week7/old_trees.py
@@ -0,0 +1,7 @@
+Oak = {"type": "old", "leaf color":"changing", "age": 100, "sex": "female"}
+Palm = {"type": "summer", "leaf color":"dark green", "age": 4, "sex": "male"}
+Coconut = {"type": "useful", "leaf color":"green", "age": 7, "sex": "female"}
+Pine = {"type": "cool", "leaf color":"light green", "age": 1, "sex": "male"}
+Christmas = {"type": "merry", "leaf color":"green", "age": 17, "sex": "female"}
+
+print(Oak["age"])
