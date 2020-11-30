c7bf413f42784cca82110e2242f85e78bd7e4788
Mark Ambrus <ambrusmark97@gmail.com>
2020-11-30 16:25:42 +0100

    3rd task

---
+++ week7/3_animal.py
@@ -0,0 +1,21 @@
+#Create an Animal class
+#Every animal has a hunger value, which is a whole number
+#Every animal has a thirst value, which is a whole number
+#when creating a new animal object these values are created with the default 50 value
+#Every animal can eat() which decreases their hunger by one
+#Every animal can drink() which decreases their thirst by one
+#Every animal can play() which increases both by one
+
+class Animal(object):
+    hunger = 50
+    thirst = 50
+
+    def eat(self):
+        self.hunger -= 1
+
+    def drink(self):
+        self.thirst -= 1
+
+    def play(self):
+        self.hunger += 1
+        self.thirst += 1
