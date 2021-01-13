5c52e7f343f37c3d12d680cdf341caccd3544d50
Mark Ambrus <ambrusmark97@gmail.com>
2021-01-13 16:34:09 +0100

    random map

--- wanderer/project/area.py
+++ wanderer/project/area.py
@@ -1,3 +1,5 @@
+from random import randrange
+
 from floor import Floor
 from wall import Wall
 from PIL import Image
@@ -13,15 +15,17 @@ class Area:
         self.number_of_tiles = 10
         self.tile_size = self.resources.floor_size
         self.tiles = []
-        self.walls = [13, 15, 17, 18, 21, 22, 23, 25, 28, 35, 41, 42, 43, 45,
-                      47, 51, 61, 63, 65, 66, 68, 75, 78, 81, 82, 83, 88, 95]
+        self.walls = [] #[13, 15, 17, 18, 21, 22, 23, 25, 28, 35, 41, 42, 43, 45,
+                      #47, 51, 61, 63, 65, 66, 68, 75, 78, 81, 82, 83, 88, 95]
+        self.floors = []
         self.size = self.tile_size * self.number_of_tiles
 
     def create_map(self):
+        self.create_walls()
         for i in range(self.number_of_tiles):
             for j in range(self.number_of_tiles):
                 tile = []
-                position = j * 10 + i
+                position = j * self.number_of_tiles + i
                 if position in self.walls:
                     tile.append(Wall())
                 else:
@@ -54,3 +58,117 @@ class Area:
 
     def increase_turn_count(self):
         self.turn_count += 1
+
+    def create_walls(self):
+        self.random_map()
+        self.connect_map()
+
+    def random_map(self):
+        self.floors.append(0)
+        for i in range(1, self.number_of_tiles ** 2):
+            random = randrange(0, 10)
+            if random < randrange(3, 7):
+                self.walls.append(i)
+            else:
+                self.floors.append(i)
+        if len(self.floors) < 6:
+            self.floors = []
+            self.walls = []
+            self.random_map()
+
+    def connect_map(self):
+        unseen = self.floors + self.walls
+        seen = [0]
+        been = [0]
+        unseen.remove(0)
+        #print('floors ' + str(self.floors))
+        #print(len(self.floors))
+        #print('walls ' + str(self.walls))
+        #print(len(self.walls))
+        #while len(self.floors) != len(been):
+        for _ in range(1):
+            #print('start')
+            self.see_neighbours(0, unseen, seen)
+            #self.print_all(unseen, seen, been)
+            self.check_seen(unseen, seen, been)
+            #self.print_all(unseen, seen, been)
+            #for _ in range(3):
+            while len(set(self.floors)) != len(set(been)):
+                #print('floors' + str(len(set(self.floors))) + 'been' + str(len(set(been))))
+                self.check_over_walls(unseen, seen, been)
+                #self.print_all(unseen, seen, been)
+                self.check_seen(unseen, seen, been)
+
+    def print_all(self, unseen, seen, been):
+        print('been on ' + str(been))
+        print(len(been))
+        print('seen ' + str(seen))
+        print(len(seen))
+        print('unseen ' + str(unseen))
+        print(len(unseen))
+
+
+    def see_neighbours(self, tile, unseen, seen):
+        #print('see neighbours of ' + str(tile))
+        if tile - 1 in unseen and tile % self.number_of_tiles != 0:
+            self.see(tile - 1, unseen, seen)
+        if tile + 1 in unseen and (tile + 1) % self.number_of_tiles != 0:
+            self.see(tile + 1, unseen, seen)
+        if tile - self.number_of_tiles in unseen:
+            self.see(tile - self.number_of_tiles, unseen, seen)
+        if tile + self.number_of_tiles in unseen:
+            self.see(tile + self.number_of_tiles, unseen, seen)
+
+    def see(self, tile, unseen, seen):
+        #print('seeing ' +str(tile))
+        seen.append(tile)
+        unseen.remove(tile)
+
+    def check_seen(self, unseen, seen, been):
+        for tile in seen:
+            #print('checking seen ' + str(tile))
+            if tile in self.floors and tile not in been:
+                been.append(tile)
+                self.see_neighbours(tile, unseen, seen)
+        #seen_as_set = set(seen)
+        #common = seen_as_set.intersection(self.floors)
+        #print('common ' + str(len(common)) + 'been_len ' + str(len(been)))
+        #if len(been) < len(common):
+            #print('hello there')
+            #self.check_seen(unseen, seen, been)
+
+    def check_over_walls(self, unseen, seen, been):
+        #print('checking over walls')
+        for tile in unseen:
+            if tile in self.floors:
+                if tile - 2 in been and tile - 1 in self.walls:
+                    self.break_wall(tile, tile - 1, unseen, seen, been)
+                    #print('break ' + str(tile - 1))
+                    return
+                elif tile - (2 * self.number_of_tiles) in been and tile - self.number_of_tiles in self.walls:
+                    self.break_wall(tile, tile - self.number_of_tiles, unseen, seen, been)
+                    #print('break ' + str(tile - self.number_of_tiles))
+                    return
+                elif tile + 2 in been and tile + 1 in self.walls:
+                    self.break_wall(tile, tile + 1, unseen, seen, been)
+                    #print('break ' + str(tile + 1))
+                    return
+                elif tile + (2 * self.number_of_tiles) in been and tile + self.number_of_tiles in self.walls:
+                    self.break_wall(tile, tile + self.number_of_tiles, unseen, seen, been)
+                    #print('break ' + str(tile + self.number_of_tiles))
+                    return
+                else:
+                    print(str(tile) + 'and' + str(unseen[len(unseen) - 1]))
+                    if tile == unseen[len(unseen) - 1]:
+                        self.make_wall(tile)
+                        #print('wall ' + str(tile))
+                    else:
+                        continue
+
+    def break_wall(self, tile, tile_between, unseen, seen, been):
+        self.walls.remove(tile_between)
+        self.floors.append(tile_between)
+
+    def make_wall(self, tile):
+        self.walls.append(tile)
+        self.floors.remove(tile)
